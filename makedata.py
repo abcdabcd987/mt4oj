3cc99fa37dd7ceececb34665d1e3378aeaca35e8
Lequn Chen <chenlequn22@gmail.com>
2018-05-03 16:51:03 +0800

    rnn: lookback

--- makedata.py
+++ makedata.py
@@ -268,12 +268,147 @@ def run_makedata_recommend(db):
     cur.close()
 
 
+def run_make_features(db):
+    with shelve.open('data/problem_features.shelf') as shelf:
+        pf = shelf['pf']
+    num_tags = len(next(iter(pf.values()))['pf_tags'])
+
+    cur = get_cur(db)
+    cur.execute('SELECT COUNT(*) as count FROM records WHERE language=%s', ('C++', ))
+    cnt = cur.fetchone()['count']
+    cur.close()
+
+    f = dict(
+        uf_num_submit=np.empty(cnt, dtype=np.int16),
+        uf_ac_rate=np.empty(cnt, dtype=np.float32),
+        uf_tag_num_submit=np.empty((cnt, num_tags), dtype=np.int16),
+        uf_tag_ac_rate=np.empty((cnt, num_tags), dtype=np.float32),
+        problem_id=np.empty(cnt, dtype=np.int16),
+        user_id=np.empty(cnt, dtype=np.int16),
+        label=np.empty(cnt, dtype=np.bool_)
+    )
+
+    cur = get_cur(db, named=True)
+    cur.execute('SELECT owner, problem_id, result FROM records WHERE language=%s ORDER BY id', ('C++', ))
+    cnt_rows = 0
+    users = {}
+    for record in tqdm(cur, total=cnt):
+        if record['result'] in ['Compile Error', 'System Error', 'Unknown']:
+            continue
+        u = users.get(record['owner'], None)
+        if u is None:
+            u = users[record['owner']] = dict(
+                num_submit=0,
+                num_ac=0,
+                num_tag_submit=np.zeros(num_tags, np.int32),
+                num_tag_ac=np.zeros(num_tags, np.int32),
+                user_id=len(users)
+            )
+        problem_id = record['problem_id']
+        p = pf.get(problem_id, None)
+        if p is None:
+            continue  # problem not exist
+
+        f['user_id'][cnt_rows] = u['user_id']
+        f['problem_id'][cnt_rows] = problem_id
+        if record['result'] == 'Accepted':
+            f['label'][cnt_rows] = 1
+            u['num_ac'] += 1
+            u['num_tag_ac'] += p['pf_tags']
+        else:
+            f['label'][cnt_rows] = 0
+        u['num_submit'] += 1
+        u['num_tag_submit'] += p['pf_tags']
+
+        f['uf_num_submit'][cnt_rows] = u['num_submit']
+        f['uf_ac_rate'][cnt_rows] = u['num_ac'] / u['num_submit'] if u['num_submit'] else 0
+        f['uf_tag_num_submit'][cnt_rows] = u['num_tag_submit']
+        f['uf_tag_ac_rate'][cnt_rows] = np_divide(u['num_tag_ac'].astype(np.float32), u['num_tag_submit'].astype(np.float32))
+        cnt_rows += 1
+    for k, v in f.items():
+        f[k] = v[:cnt_rows]
+
+    np.savez('data/features.npz', num_rows=cnt_rows, num_users=len(users), **f)
+    cur.close()
+
+
+def run_makedata_from_features():
+    features = [
+        'uf_num_submit',
+        'uf_ac_rate',
+        'uf_tag_num_submit',
+        'uf_tag_ac_rate',
+        'pf_num_submit',
+        'pf_ac_rate',
+        'pf_avg_lines',
+        'pf_avg_bytes',
+        'pf_avg_time',
+        'pf_avg_mem',
+        'pf_avg_score',
+        'pf_tags'
+    ]
+
+    with shelve.open('data/problem_features.shelf') as shelf:
+        pf = shelf['pf']
+    sample_pf = next(iter(pf.values()))
+    num_tags = len(sample_pf['pf_tags'])
+    with np.load('data/features.npz') as data:
+        f = dict(
+            uf_num_submit=data['uf_num_submit'],
+            uf_ac_rate=data['uf_ac_rate'],
+            uf_tag_num_submit=data['uf_tag_num_submit'],
+            uf_tag_ac_rate=data['uf_tag_ac_rate'],
+            problem_id=data['problem_id'],
+            user_id=data['user_id'],
+            label=data['label'],
+        )
+        num_rows = int(data['num_rows'])
+        num_users = int(data['num_users'])
+
+    length = 0
+    for name in features:
+        if name.startswith('uf_'):
+            sample = f[name][0]
+        elif name.startswith('pf_'):
+            sample = sample_pf[name]
+        else:
+            assert False
+        if type(sample) is np.ndarray:
+            length += len(sample)
+        else:
+            length += 1
+
+    x = np.empty((num_rows, length), dtype=np.float32)
+    y = np.empty(num_rows, dtype=np.int8)
+
+    for row in tqdm(range(num_rows)):
+        offset = 0
+        p = pf[f['problem_id'][row]]
+        for name in features:
+            if name.startswith('uf_'):
+                value = f[name][row]
+            elif name.startswith('pf_'):
+                value = p[name]
+            else:
+                assert False
+            if type(value) is np.ndarray:
+                flen = len(value)
+            else:
+                flen = 1
+            x[row, offset:offset+flen] = value
+            offset += flen
+        y[row] = f['label'][row]
+    np.savez('data/data.npz', x=x, y=y)
+
+
 
 def main():
     db = get_db()
     # run_make_problem_features(db)
-    run_makedata(db)
+    # run_makedata(db)
     # run_makedata_recommend(db)
+    # run_make_features(db)
+    run_makedata_from_features()
 
 
 if __name__ == '__main__':
--- run_rnn.py
+++ run_rnn.py
@@ -1,47 +1,116 @@
 import os
 os.environ['KERAS_BACKEND'] = 'tensorflow'
+import bisect
 import copy
 import random
 import shelve
 import shutil
 import numpy as np
+from tqdm import tqdm
 from keras.models import Sequential, load_model
 from keras.layers import Dense, LSTM
+from keras.utils import Sequence
 from sklearn.metrics import accuracy_score, roc_auc_score
 from sklearn.preprocessing import StandardScaler, MaxAbsScaler
 
 
+class InputSequenceSharedData:
+    def __init__(self, *, frac_train):
+        print('InputSequenceSharedData: reading data..')
+        with np.load('data/features.npz') as data:
+            self.data_user_ids = data['user_id']
+            self.num_rows = int(data['num_rows'])
+            self.num_users = int(data['num_users'])
+        with np.load('data/data.npz') as data:
+            self.data_x, self.data_y = data['x'], data['y']
+
+        print('InputSequenceSharedData: scaling features...')
+        self.scalar = MaxAbsScaler().fit(self.data_x)
+        self.data_x = self.scalar.transform(self.data_x)
+
+        print('InputSequenceSharedData: building data structures...')
+        self.num_train = int(self.num_rows * frac_train)
+        self.train_user_rows = [[] for _ in range(self.num_users)]
+        self.test_user_rows = [[] for _ in range(self.num_users)]
+        for row in range(self.num_train):
+            user_id = self.data_user_ids[row]
+            self.train_user_rows[user_id].append(row)
+        for row in range(self.num_train, self.num_rows):
+            user_id = self.data_user_ids[row]
+            self.test_user_rows[user_id].append(row)
+
+        random_state = np.random.RandomState(1234)
+        self.perm = random_state.permutation(self.num_users)
+        self.train_user_offsets, self.test_user_offsets = [0], [0]
+        for user_id in self.perm:
+            self.train_user_offsets.append(self.train_user_offsets[-1] + len(self.train_user_rows[user_id]))
+            self.test_user_offsets.append(self.test_user_offsets[-1] + len(self.test_user_rows[user_id]))
+        self.train_user_offsets = self.train_user_offsets[1:]
+        self.test_user_offsets = self.test_user_offsets[1:]
+
+        print('InputSequenceSharedData: init done')
+
+
+class InputSequence(Sequence):
+    def __init__(self, data, *, is_train, batch_size, lookback):
+        self._data = data
+        self._is_train = is_train
+        self._batch_size = batch_size
+        self._lookback = lookback
+        self._total_rows = self._data.num_train if self._is_train else self._data.num_rows - self._data.num_train
+
+    def __len__(self):
+        return int(np.ceil(self._total_rows / self._batch_size))
+
+    def __getitem__(self, batch_idx):
+        start_idx = batch_idx * self._batch_size
+        end_idx = min((batch_idx+1) * self._batch_size, self._total_rows)
+        ret_rows = end_idx - start_idx
+        batch_x = np.zeros((ret_rows, self._lookback+1, self._data.data_x.shape[1]), dtype=np.float32)
+        batch_y = np.empty(ret_rows, dtype=np.int8)
+        user_offsets = self._data.train_user_offsets if self._is_train else self._data.test_user_offsets
+        user_rows = self._data.train_user_rows if self._is_train else self._data.test_user_rows
+        for i, idx in enumerate(range(start_idx, end_idx)):
+            user_idx = bisect.bisect_right(user_offsets, idx)
+            user_id = self._data.perm[user_idx]
+            user_row_offset = idx - user_offsets[user_idx-1] if user_idx != 0 else idx
+            for t in range(self._lookback+1):
+                if user_row_offset-t >= 0:
+                    row = user_rows[user_id][user_row_offset]
+                    batch_x[i, t] = self._data.data_x[row]
+                elif not self._is_train:
+                    train_rows = self._data.train_user_rows[user_id]
+                    row_offset = len(train_rows) + (user_row_offset - t)
+                    if row_offset >= 0:
+                        row = train_rows[row_offset]
+                        batch_x[i, t] = self._data.data_x[row]
+                # otherwise just fill 0
+            row = user_rows[user_id][user_row_offset]
+            batch_y[i] = self._data.data_y[row]
+        return batch_x, batch_y
+
+
 def run_train_test():
-    print('reading data...')
-    with np.load('data/data.npz') as data:
-        x_all, y_all = data['x'], data['y']
+    batch_size = 256
+    lookback = 5
 
-    print('transforming data...')
-    y_all[y_all < 0] = 0
-
-    # split train and test
-    print('shuffling training data...')
-    num_train = int(len(y_all) * 0.8)
-    x_train, y_train = x_all[:num_train], y_all[:num_train]
-    x_test, y_test = x_all[num_train:], y_all[num_train:]
-    # perm = np.random.permutation(num_train)
-    # x_train, y_train = x_train[perm], y_train[perm]
-
-    # feature scaling
-    print('scaling features...')
-    scalar = MaxAbsScaler().fit(x_train)
-    x_train = scalar.transform(x_train)
-    x_test = scalar.transform(x_test)
-
-    # reshape to 3D
-    print('reshaping input matrix to 3D...')
-    x_train = x_train.reshape((x_train.shape[0], 1, x_train.shape[1]))
-    x_test = x_test.reshape((x_test.shape[0], 1, x_test.shape[1]))
+    data = InputSequenceSharedData(frac_train=0.8)
+    train_input_seq = InputSequence(data, is_train=True, batch_size=batch_size, lookback=lookback)
+    test_input_seq = InputSequence(data, is_train=False, batch_size=batch_size, lookback=lookback)
+    batch_x, batch_y = train_input_seq[20]
+    x_test, y_test = [], []
+    for i in range(len(test_input_seq)):
+        batch_x, batch_y = test_input_seq[i]
+        x_test.append(batch_x)
+        y_test.append(batch_y)
+    x_test = np.concatenate(x_test)
+    y_test = np.concatenate(y_test)
+    print(x_test.shape, y_test.shape)
 
     # keras: build graph
     print('building graph...')
     model = Sequential()
-    model.add(LSTM(100, input_shape=x_train.shape[1:]))
+    model.add(LSTM(100, input_shape=(lookback+1, data.data_x.shape[1])))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     model.summary()
@@ -49,7 +118,10 @@ def run_train_test():
     # keras: train model
     print('training...')
     for epoch in range(1, 13):
-        model.fit(x_train, y_train, epochs=1, batch_size=128, verbose=1)
+        model.fit_generator(train_input_seq,
+            epochs=1, verbose=1,
+            use_multiprocessing=True, workers=2,
+            shuffle=False)
         model.save('data/rnn/model.h5')
         shutil.copy('data/rnn/model.h5', 'data/rnn/model-epoch-{}.h5'.format(epoch))
 
